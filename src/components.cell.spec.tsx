b52761bede5c1c095e65fab4ba774e2783389e49
kaiser shahid <kaisershahid@gmail.com>
2026-03-08 13:08:18 -0400

    - more components.cell.Cell overage

--- src/components.cell.spec.tsx
+++ src/components.cell.spec.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Cell, CellEdit } from "./components.cell";
 import { EMPTY_ROW_CONTEXT } from "./types";
 
 describe('module: components.cell', () => {
-    describe.only('Cell', () => {
+    describe('Cell', () => {
         it('renders Cell {children}', () => {
             render(<Cell>test</Cell>);
 
@@ -16,7 +16,7 @@ describe('module: components.cell', () => {
 
             const bCell = screen.getByText('test');
             expect(bCell.tagName.toLowerCase()).toEqual("b");
-            
+
             const divCell = bCell.parentElement;
             expect(divCell).toHaveClass("test-class");
             expect(divCell?.dataset.ezdgCell).toEqual("2")
@@ -34,9 +34,41 @@ describe('module: components.cell', () => {
             expect(after).toBeInTheDocument();
             expect(after).toHaveClass("after");
         })
-
-        // @todo validate on* handlers
-        // @todo validate {tagIndex, decorators, id}
+        it('renders Cell {tabIndex, decorators, id}', () => {
+            render(<Cell tabIndex={1} decorators="wrap" id="c1">test</Cell>);
+            const htmlCell = screen.getByText('test');
+            expect(htmlCell).toBeInTheDocument();
+            expect(htmlCell.tabIndex).toEqual(1)
+            expect(htmlCell.dataset.ezdgDecorators).toEqual("wrap")
+            expect(htmlCell.dataset.ezdgColindex).toEqual("c1")
+        })
+        it('renders Cell {onClick, onDoubleClick, onKeyDown, onKeyUp, onMouseEnter, onMouseOver, onMouseLeave}', () => {
+            const callMap: Record<string, boolean> = {};
+            const onClick = () => { callMap['onClick'] = true };
+            const onDoubleClick = () => { callMap['onDoubleClick'] = true };
+            const onKeyDown = () => { callMap['onKeyDown'] = true };
+            const onKeyUp = () => { callMap['onKeyUp'] = true };
+            const onMouseEnter = () => { callMap['onMouseEnter'] = true };
+            const onMouseOver = () => { callMap['onMouseOver'] = true };
+            const onMouseLeave = () => { callMap['onMouseLeave'] = true };
+            
+            render(<Cell onClick={onClick} onDoubleClick={onDoubleClick} onKeyDown={onKeyDown} onKeyUp={onKeyUp} onMouseEnter={onMouseEnter} onMouseOver={onMouseOver} onMouseLeave={onMouseLeave}>test</Cell>);
+            const htmlCell = screen.getByText('test');
+            fireEvent.click(htmlCell);
+            fireEvent.doubleClick(htmlCell);
+            fireEvent.keyDown(htmlCell);
+            fireEvent.keyUp(htmlCell);
+            fireEvent.mouseEnter(htmlCell);
+            fireEvent.mouseOver(htmlCell);
+            fireEvent.mouseLeave(htmlCell);
+            expect(callMap['onClick']).toBe(true);
+            expect(callMap['onDoubleClick']).toBe(true);
+            expect(callMap['onKeyDown']).toBe(true);
+            expect(callMap['onKeyUp']).toBe(true);
+            expect(callMap['onMouseEnter']).toBe(true);
+            expect(callMap['onMouseOver']).toBe(true);
+            expect(callMap['onMouseLeave']).toBe(true);
+        })
     });
     describe('CellEdit', () => {
         it('renders CellEdit {children}', () => {
