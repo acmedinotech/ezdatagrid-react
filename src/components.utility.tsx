10fb5c10fb266b578be60c2319cea0c95943b3ad
kaiser shahid <kaisershahid@gmail.com>
2026-03-20 21:50:47 -0400

    fixup! - basic Modal & bulk edit popup

--- src/components.utility.tsx
+++ src/components.utility.tsx
@@ -1,5 +1,11 @@
 export const Modal = ({ closeFn, children }: { closeFn: () => void } & React.PropsWithChildren) => {
-    return <div data-ezdg-backdrop>
+    return <div data-ezdg-backdrop onClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+    }} onDoubleClick={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+    }}>
         <div data-ezdg-modal>
             <div data-ezdg-toolbar="right"><button onClick={() => closeFn()}>close</button></div>
             {children}
